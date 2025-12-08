ae2745dbdd20bfbc64ca7fdcc43023156bdf6945
Nils Fahldieck <nils.fahldieck@ibm.com>
2025-12-08 15:57:14 +0100

    Add HTTP health_checks for Terraform modules

--- hcp-workspace/main.tf
+++ hcp-workspace/main.tf
@@ -27,3 +27,26 @@ module "azure_onepager" {
   vm_size             = var.azure_vm_size
   vnet_address_prefix = var.azure_vnet_address_prefix
 }
+
+check "aws_health_check" {
+  data "http" "aws_onepager" {
+    url = module.aws_onepager.website_url
+  }
+
+  assert {
+    condition     = data.http.aws_onepager.status_code == 200
+    error_message = "${data.http.aws_onepager.url} (AWS) returned an unhealthy status code (${data.http.aws_onepager.status_code})"
+  }
+}
+
+check "azure_health_check" {
+  data "http" "azure_onepager" {
+    url = module.azure_onepager.website_url
+  }
+
+  assert {
+    condition     = data.http.azure_onepager.status_code == 200
+    error_message = "${data.http.azure_onepager.url} (Azure) returned an unhealthy status code (${data.http.azure_onepager.status_code})"
+  }
+}
+
